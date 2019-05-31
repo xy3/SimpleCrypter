3c84ad8f8587d65ea509b09aac3b39870c1cf29e
0xDepp <a@xsq.pw>
2019-05-31 02:46:56 +0100

    Add project files.

---
+++ SimpleCrypter/SimpleCrypter.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace SimpleCrypter
+{
+    partial class SimpleCrypter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filedialog = new System.Windows.Forms.OpenFileDialog();
+            this.btn_encrypt = new System.Windows.Forms.Button();
+            this.tb_file = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.tb_outdir = new System.Windows.Forms.TextBox();
+            this.btn_file = new System.Windows.Forms.Button();
+            this.btn_outfile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_randpw = new System.Windows.Forms.Button();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.folder_dialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_outfile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // filedialog
+            // 
+            this.filedialog.FileName = "File to encrypt";
+            this.filedialog.Filter = "Executable Files (*.exe;*.dll)|*.exe;*.dll";
+            // 
+            // btn_encrypt
+            // 
+            this.btn_encrypt.BackColor = System.Drawing.Color.Black;
+            this.btn_encrypt.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_encrypt.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_encrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_encrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_encrypt.ForeColor = System.Drawing.Color.White;
+            this.btn_encrypt.Location = new System.Drawing.Point(301, 207);
+            this.btn_encrypt.Name = "btn_encrypt";
+            this.btn_encrypt.Size = new System.Drawing.Size(80, 26);
+            this.btn_encrypt.TabIndex = 0;
+            this.btn_encrypt.Text = "Encrypt";
+            this.btn_encrypt.UseVisualStyleBackColor = false;
+            this.btn_encrypt.Click += new System.EventHandler(this.btn_encrypt_Click);
+            // 
+            // tb_file
+            // 
+            this.tb_file.BackColor = System.Drawing.Color.Black;
+            this.tb_file.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_file.ForeColor = System.Drawing.Color.White;
+            this.tb_file.Location = new System.Drawing.Point(106, 72);
+            this.tb_file.Name = "tb_file";
+            this.tb_file.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tb_file.Size = new System.Drawing.Size(189, 26);
+            this.tb_file.TabIndex = 1;
+            this.tb_file.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tb_password
+            // 
+            this.tb_password.BackColor = System.Drawing.Color.Black;
+            this.tb_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_password.ForeColor = System.Drawing.Color.White;
+            this.tb_password.Location = new System.Drawing.Point(106, 117);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(189, 26);
+            this.tb_password.TabIndex = 2;
+            // 
+            // tb_outdir
+            // 
+            this.tb_outdir.BackColor = System.Drawing.Color.Black;
+            this.tb_outdir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_outdir.ForeColor = System.Drawing.Color.White;
+            this.tb_outdir.Location = new System.Drawing.Point(106, 162);
+            this.tb_outdir.Name = "tb_outdir";
+            this.tb_outdir.Size = new System.Drawing.Size(189, 26);
+            this.tb_outdir.TabIndex = 3;
+            this.tb_outdir.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btn_file
+            // 
+            this.btn_file.BackColor = System.Drawing.Color.Black;
+            this.btn_file.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_file.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_file.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_file.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_file.ForeColor = System.Drawing.Color.White;
+            this.btn_file.Location = new System.Drawing.Point(301, 72);
+            this.btn_file.Name = "btn_file";
+            this.btn_file.Size = new System.Drawing.Size(80, 26);
+            this.btn_file.TabIndex = 4;
+            this.btn_file.Text = "Choose";
+            this.btn_file.UseVisualStyleBackColor = false;
+            this.btn_file.Click += new System.EventHandler(this.btn_file_Click);
+            // 
+            // btn_outfile
+            // 
+            this.btn_outfile.BackColor = System.Drawing.Color.Black;
+            this.btn_outfile.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_outfile.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_outfile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_outfile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_outfile.ForeColor = System.Drawing.Color.White;
+            this.btn_outfile.Location = new System.Drawing.Point(301, 162);
+            this.btn_outfile.Name = "btn_outfile";
+            this.btn_outfile.Size = new System.Drawing.Size(80, 26);
+            this.btn_outfile.TabIndex = 5;
+            this.btn_outfile.Text = "Choose";
+            this.btn_outfile.UseVisualStyleBackColor = false;
+            this.btn_outfile.Click += new System.EventHandler(this.btn_outfile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(106, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "File";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(106, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(106, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Output Dir";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // btn_randpw
+            // 
+            this.btn_randpw.BackColor = System.Drawing.Color.Black;
+            this.btn_randpw.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_randpw.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_randpw.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_randpw.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_randpw.ForeColor = System.Drawing.Color.White;
+            this.btn_randpw.Location = new System.Drawing.Point(301, 117);
+            this.btn_randpw.Name = "btn_randpw";
+            this.btn_randpw.Size = new System.Drawing.Size(80, 26);
+            this.btn_randpw.TabIndex = 9;
+            this.btn_randpw.Text = "Random";
+            this.btn_randpw.UseVisualStyleBackColor = false;
+            this.btn_randpw.Click += new System.EventHandler(this.btn_randpw_Click);
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.BackColor = System.Drawing.Color.Black;
+            this.lbl_status.ForeColor = System.Drawing.Color.White;
+            this.lbl_status.Location = new System.Drawing.Point(106, 247);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(37, 13);
+            this.lbl_status.TabIndex = 10;
+            this.lbl_status.Text = "Status";
+            // 
+            // folder_dialog
+            // 
+            this.folder_dialog.Description = "Select output location";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(106, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Output filename";
+            // 
+            // tb_outfile
+            // 
+            this.tb_outfile.BackColor = System.Drawing.Color.Black;
+            this.tb_outfile.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_outfile.ForeColor = System.Drawing.Color.White;
+            this.tb_outfile.Location = new System.Drawing.Point(106, 207);
+            this.tb_outfile.Name = "tb_outfile";
+            this.tb_outfile.Size = new System.Drawing.Size(189, 26);
+            this.tb_outfile.TabIndex = 12;
+            this.tb_outfile.Text = "crypted.exe";
+            // 
+            // SimpleCrypter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(465, 308);
+            this.Controls.Add(this.tb_outfile);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.btn_randpw);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_outfile);
+            this.Controls.Add(this.btn_file);
+            this.Controls.Add(this.tb_outdir);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_file);
+            this.Controls.Add(this.btn_encrypt);
+            this.Name = "SimpleCrypter";
+            this.Text = "SimpleCrypter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog filedialog;
+        private System.Windows.Forms.Button btn_encrypt;
+        private System.Windows.Forms.TextBox tb_file;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.TextBox tb_outdir;
+        private System.Windows.Forms.Button btn_file;
+        private System.Windows.Forms.Button btn_outfile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_randpw;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.FolderBrowserDialog folder_dialog;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_outfile;
+    }
+}
+
---
+++ SimpleCrypter/SimpleCrypter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SimpleCrypter
+{
+    public partial class SimpleCrypter : Form
+    {
+        public SimpleCrypter()
+        {
+            InitializeComponent();
+            tb_password.Text = RandomString(15);
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void btn_encrypt_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(tb_file.Text))
+                lbl_status.Text = "The file does not exist.";
+            if (tb_outfile.Text.Length == 0)
+                lbl_status.Text = "Enter an output filename.";
+            if (tb_password.Text.Length == 0)
+                lbl_status.Text = "Enter a password.";
+
+            try
+            {
+                encrypt(tb_file.Text, tb_password.Text, tb_outdir.Text);
+                lbl_status.Text = "Successfully encrypted the file.";
+            }
+            catch (Exception)
+            {
+                lbl_status.Text = "Failed to encrypt the file.";
+            }
+        }
+
+        private void encrypt(String file, String pass, String outfile)
+        {
+            byte[] plainBytes = File.ReadAllBytes(file);
+            byte[] encodedBytes = encodeBytes(plainBytes, pass);
+
+            File.WriteAllBytes(outfile, encodedBytes);
+        }
+
+        private static byte[] encodeBytes(byte[] bytes, String pass)
+        {
+            byte[] XorBytes = Encoding.Unicode.GetBytes(pass);
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] ^= XorBytes[i % 16];
+            }
+
+            return bytes;
+        }
+
+        private void btn_randpw_Click(object sender, EventArgs e)
+        {
+            tb_password.Text = RandomString(15);
+        }
+
+        private void btn_file_Click(object sender, EventArgs e)
+        {
+            if (filedialog.ShowDialog() == DialogResult.OK)
+            {
+                tb_file.Text = filedialog.FileName;
+            }
+        }
+
+        private void btn_outfile_Click(object sender, EventArgs e)
+        {
+            if (folder_dialog.ShowDialog() == DialogResult.OK)
+            {
+                tb_outdir.Text = folder_dialog.SelectedPath;
+            }
+        }
+    }
+}
+
+
